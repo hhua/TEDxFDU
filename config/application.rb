1af2d740d4ce9027f9475c64cd815a6f22a16dee
chinesecold <chinesecold@gmail.com>
2013-01-13 23:49:12 -0500

    Configured for deployment to Heroku

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,11 @@ module RailsTry
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Make sure Bootstrap.css is precompiled
+    config.assets.precompile += %w( bootstrap.css )
+
+    # Heroku requires this to be false
+    config.assets.initialize_on_precompile=false
   end
 end
