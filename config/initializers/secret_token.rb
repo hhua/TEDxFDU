76266bf9e63f6c1177a007d4a5be970c3b9b48ff
chinesecold <chinesecold@gmail.com>
2013-01-13 20:46:24 -0500

    Rails refactoring

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTry::Application.config.secret_token = '8d59cde5411dae84614b2b6587e88cdeb57b9da07df99ae3bd19527f25fa2ecab2e4d518c6ad1d3156c2944570ba8adc4548625f050d2e6e48f199dfd20df453'
